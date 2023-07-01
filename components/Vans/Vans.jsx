b778090c1ad222fbe4710d84dd1eb2c87231c000
YuraShum <91155294+YuraShum@users.noreply.github.com>
2023-07-01 22:15:07 +0300

    Add component Vans

--- components/Vans/Vans.jsx
+++ components/Vans/Vans.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react"
+
 export default function Vans(){
+
+    const [vans, setVans] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/vans")
+        .then(result => result.json())
+        .then(data => setVans(data.vans))
+    }, [])
+
+    const vanElements = vans.map(van => {
+        return(
+            <div className="van-content" key={van.id}>
+                <img src={van.imageUrl} alt="" />
+                <div className="van-information">
+                    <h3>{van.name}</h3>
+                    <p>{van.price}<span>/day</span></p>
+                    <i>{van.type}</i>
+                </div>
+            </div>
+        )
+    })
     return(
         <div>
-            <h1>Vans page</h1>
+            <h2>Explorte our van options</h2>
+            {vanElements}
         </div>
     )
 }
