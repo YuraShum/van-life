c3d699158248fd4e6d784db01a03cda822df8701
YuraShum <91155294+YuraShum@users.noreply.github.com>
2023-07-01 16:28:38 +0300

    update index file

--- index.jsx
+++ index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import About from './components/pages/About/About.jsx';
-import Home from './components/pages/Home/Home.jsx';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import About from './components/About/About.jsx';
+import Home from './components/Home/Home.jsx';
 
 
 import "./server.js";
@@ -11,31 +11,31 @@ import "./server.js";
 function App() {
   return (
     <BrowserRouter >
-    <header className='container'>
-      <nav className='navigation'>
-        <Link to='/'>#Vanlife</Link>
-        <div className="link-section">
-          <Link to='/about'>About</Link>
-          <Link to='/vans'>Vans</Link>
-        </div>
-      </nav>
-    </header>
-    <Routes>
-      <Route
-        path="/"
-        element={<Home/>}
-      />
-      <Route
-        path="/about"
-        element={<About/>}
-      />
-    </Routes>
-    
-  </BrowserRouter>
+      <header className='container'>
+        <nav className='navigation'>
+          <Link to='/'>#Vanlife</Link>
+          <div className="link-section">
+            <Link to='/about'>About</Link>
+            <Link to='/vans'>Vans</Link>
+          </div>
+        </nav>
+      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App/>
+  <App />
 
-  );
+);
